97897c558cab203c5655cb0e4fe9fd12a91fc261
letnull19a <alexvolkovdd@gmail.com>
2024-11-12 23:52:00 +0300

    feat: добавил страницу входа

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,19 @@
+import {Panel} from './../../widgets'
+import {Link} from 'react-router-dom'
+import './styles.module.css'
+
+const Login = () => {
+ return (
+ <Panel>
+  <h2>Войти</h2>
+   <form>
+    <input type='text' placeholder='Введите логин'/>
+    <input type='password' placeholder='Введите пароль'/>
+    <input type='submit' value='Войти'/>
+   </form>
+   <Link to='/registrstion'>Создать аккаунт</Link>
+  </Panel>
+ )
+}
+
+export default Login
